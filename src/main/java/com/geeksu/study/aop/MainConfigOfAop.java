3f796099a8017d825e58c2f1159a1e1f903ba525
robin-su <549570881@qq.com>
2018-11-28 22:56:58 +0800

    new

--- src/main/java/com/geeksu/study/aop/MainConfigOfAop.java
+++ src/main/java/com/geeksu/study/aop/MainConfigOfAop.java
@@ -126,7 +126,20 @@ import org.springframework.context.annotation.Import;
 	2)创建对象
 	postProcessAfterInitialization
 		return wrapIfNecessary(bean,beanName,cacheKey);包装如果需要的话
-		1） 获取当前bean的所有增强器（通知）
+		1）获取当前bean的所有增强器（通知方法）Object[] specificInterceptors
+			1.找到候选的所有的增强器（找哪些通知方法是需要切入当前bean的方法）
+			2.获取到能在bean使用的增强器
+			3.给增强器排序
+		2）保存当前bean在advisedBeans中
+		3）如果当前bean需要增强，创建当前bean的代理对象：
+			1） 获取当前bean的所有增强器（通知方法）
+			2） 保存到proxyFactory
+			3)  创建代理对象:Spring自定决定
+				JdkDynamicAopProxy(config);jdk动态代理
+				ObjenesisCglibAopProxy(config)；cglib的动态代理
+		4）给容器中返回当前组件使用cglib增强了的代理对象
+		5） 以后容器中获取到的就是这个组件的代理对象，执行目标方法的时候，代理对象就会执行通知方法的流程
+
  */
 @EnableAspectJAutoProxy
 @Configuration
